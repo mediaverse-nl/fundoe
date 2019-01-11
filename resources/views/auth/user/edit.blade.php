9a6acd156604c088bf16765e3011c14c2c5d5d08
master <deveron-reniers@hotmail.com>
2019-01-11 16:58:47 +0100

    admin panel update

--- resources/views/auth/user/edit.blade.php
+++ resources/views/auth/user/edit.blade.php
@@ -21,20 +21,20 @@
                             {!! Form::label('email', 'E-Mail adres') !!}
                             {!! Form::text('email', null, ['class' => 'form-control', 'disabled']) !!}
                         </div>
-                        <div class="form-group {!! !$errors->has('watchwoord') ? : 'has-error'!!} has-warning">
+                        <div class="form-group">
                             {!! Form::label('watchwoord', 'watchwoord') !!}
-                            {!! Form::password('watchwoord', ['class' => 'form-control'. !$errors->has('watchwoord') ? : ' is_invalid']) !!}
+                            {!! Form::password('watchwoord', ['class' => 'form-control', !$errors->has('watchwoord') ? : 'required']) !!}
                             @include('components.error', ['field' => 'watchwoord'])
                         </div>
                         <div class="form-group {!! !$errors->has('herhaal_watchwoord') ? : 'has-error'!!}">
-                            {!! Form::label('herhaal_watchwoord', 'herhaal_watchwoord') !!}
+                            {!! Form::label('herhaal_watchwoord', 'herhaal watchwoord') !!}
                             {!! Form::password('herhaal_watchwoord', ['class' => 'form-control']) !!}
                             @include('components.error', ['field' => 'herhaal_watchwoord'])
                         </div>
-                        {{--<div class="form-group">--}}
-                            {{--<label for="exampleInputPassword1">Password</label>--}}
-                            {{--<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">--}}
-                        {{--</div>--}}
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control is-valid" id="exampleInputPassword1" placeholder="Password">
+                        </div>
                         {{--<div class="form-check">--}}
                             {{--<input type="checkbox" class="form-check-input" id="exampleCheck1">--}}
                             {{--<label class="form-check-label" for="exampleCheck1">Check me out</label>--}}
--- resources/views/components/error.blade.php
+++ resources/views/components/error.blade.php
@@ -1,3 +1,5 @@
-@if($errors->first($field))
-    {!! $errors->first($field, '<p><span class="text-warning">:message</span></p>') !!}
-@endif
+<div class="invalid-feedback">
+    @if($errors->first($field))
+        {!! $errors->first($field, '<p><span class="text-warning">:message</span></p>') !!}
+    @endif
+</div>
